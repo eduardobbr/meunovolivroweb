19a19de2a8cb3f396cffe49c3baaf8780f6bc38d
Guilherme Gonçalves <gcgoncalves13@gmail.com>
2023-09-13 17:14:03 -0300

    feat: responsive second banner

--- src/components/SecondBanner/index.jsx
+++ src/components/SecondBanner/index.jsx
@@ -2,15 +2,22 @@ import { Link } from "react-router-dom";
 import {
   CheckoutContent,
   Container,
+  FirstImg,
+  ImgContent,
   OfferContent,
   Paragraph,
+  SecondImg,
   Size,
   SubTitle,
   Title,
   Weight,
 } from "./style";
+import { useMediaQuery } from "@material-ui/core";
+import { theme } from "../../styles/theme";
 
 const SecondBanner = () => {
+  const mobile = useMediaQuery(theme.breakpoints.up("md"));
+
   return (
     <Container>
       <OfferContent>
@@ -21,11 +28,25 @@ const SecondBanner = () => {
         </Title>
         <SubTitle>R$280</SubTitle>
       </OfferContent>
+      {mobile ? (
+        <ImgContent>
+          <FirstImg
+            src="http://localhost:1337/uploads/book_image02_ac34c820e9.png"
+            alt="Livro de divulgação"
+          />
+          <SecondImg
+            src="http://localhost:1337/uploads/book_image01_b6cc584c88.png"
+            alt="Livro de divulgação"
+          />
+        </ImgContent>
+      ) : (
+        <></>
+      )}
 
       <CheckoutContent>
         <Title>Poucos passos para publicas sua obra</Title>
         <Paragraph>
-          <Link>Comece agora</Link>
+          <Link to="/plans">Comece agora</Link>
         </Paragraph>
       </CheckoutContent>
     </Container>
--- src/components/SecondBanner/style.js
+++ src/components/SecondBanner/style.js
@@ -8,13 +8,32 @@ export const Container = styled.section`
   background-color: var(--tertiaryColor);
   color: var(--secondaryTextColor);
   padding: 30px;
+  margin-bottom: 30px;
+
+  @media only screen and (min-width: 960px) {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 50px;
+  }
 `;
 
 export const OfferContent = styled.div`
   margin: 20px 0;
+  @media only screen and (min-width: 960px) {
+    width: 20%;
+  }
 `;
 
-export const CheckoutContent = styled.div``;
+export const CheckoutContent = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media only screen and (min-width: 960px) {
+    width: 40%;
+  }
+`;
 
 export const Title = styled.h2`
   font-family: "Bitter", sans-serif;
@@ -37,22 +56,39 @@ export const SubTitle = styled.h3`
 
 export const Paragraph = styled.p`
   margin: 30px 0 0 0;
-
+  font-size: 14px;
+  border: 1px solid var(--secondaryTextColor);
+  border-bottom: 4px solid var(--secondaryTextColor);
+  text-transform: uppercase;
+  font-weight: 300;
+  transition: 300ms ease-in-out;
+  border-radius: 2px;
+  text-align: center;
+  cursor: pointer;
+  padding: 15px 25px;
   a {
-    font-size: 14px;
     color: var(--secondaryTextColor);
-    border: 1px solid var(--secondaryTextColor);
-    border-bottom: 4px solid var(--secondaryTextColor);
-    text-transform: uppercase;
-    padding: 15px 25px;
-    font-weight: 300;
-    transition: 300ms ease-in-out;
-    border-radius: 2px;
-    width: 175px;
-    text-align: center;
   }
-
-  a:hover {
+  &:hover {
     background-color: var(--quaternaryColor);
   }
+`;
+
+export const ImgContent = styled.div`
+  position: relative;
+  width: 20%;
+`;
+
+export const FirstImg = styled.img`
+  position: absolute;
+  left: -100px;
+  top: -100px;
+  height: 350px;
+`;
+
+export const SecondImg = styled.img`
+  position: absolute;
+  left: 0;
+  top: -80px;
+  height: 310px;
 `;
