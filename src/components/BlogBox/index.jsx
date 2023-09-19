57126ac4d18e2ee37eed4e12bb9e96cfa895b69a
Guilherme Gonçalves <gcgoncalves13@gmail.com>
2023-09-19 12:46:16 -0300

    feat: blogBox

--- src/components/BlogBox/index.jsx
+++ src/components/BlogBox/index.jsx
@@ -1,9 +1,13 @@
-import { Container, Title } from "./style";
+import { Container, TextBox, Title } from "./style";
 
 const BlogBox = () => {
   return (
     <Container>
       <Title>Blog</Title>
+      <TextBox>
+        There are many variations of passages of Lorem Ipsum means available but
+        suffered alteration
+      </TextBox>
     </Container>
   );
 };
--- src/components/BlogBox/style.js
+++ src/components/BlogBox/style.js
@@ -4,8 +4,13 @@ export const Container = styled.section`
   width: 80%;
   max-width: 1200px;
   margin: 10px auto;
+  padding: 50px 10px;
 `;
 
 export const Title = styled.h2`
   color: var(--primaryColor);
+`;
+
+export const TextBox = styled.p`
+  color: var(--mainTextColor);
 `;
