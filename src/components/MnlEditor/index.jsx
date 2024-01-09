793cae49bbb3e5affb6e7de983652da15f4715ae
Guilherme Gonçalves <gcgoncalves13@gmail.com>
2024-01-09 16:53:32 -0300

    feat: add make bold and italic functions

--- src/components/MnlEditor/index.jsx
+++ src/components/MnlEditor/index.jsx
@@ -3,9 +3,9 @@ import { BodyEditor, Container, EditorTitle, HeadEditor } from "./style";
 
 const MnlEditor = () => {
   const [editorText, setEditorText] = useState("");
+  const [startCount, setStartCount] = useState();
   const [target, setTarget] = useState();
   const [endCount, setEndCount] = useState();
-  const [startCount, setStartCount] = useState();
 
   const editor = useRef(null);
 
@@ -16,6 +16,32 @@ const MnlEditor = () => {
     }
   };
 
+  const filterAndAdd = (start, end, str) => {
+    return (
+      editorText.slice(0, start) +
+      str +
+      editorText.slice(end, editorText.length)
+    );
+  };
+
+  const makeItBold = (start, end) => {
+    if (start !== end) {
+      const getText = editorText.slice(start, end);
+      const textBold = `<b>${getText}</b>`;
+      setEditorText(filterAndAdd(start, end, textBold));
+    }
+    // document.execCommand("bold");
+  };
+
+  const makeItItalic = (start, end) => {
+    if (start !== end) {
+      const getText = editorText.slice(start, end);
+      const textBold = `<i>${getText}</i>`;
+      setEditorText(filterAndAdd(start, end, textBold));
+    }
+    // document.execCommand("italic");
+  };
+
   useEffect(() => {
     setTarget(editor.current);
   }, [editorText]);
@@ -24,15 +50,18 @@ const MnlEditor = () => {
     <Container>
       <HeadEditor>
         <EditorTitle>Editor Meu Novo Livro</EditorTitle>
+        <button onClick={() => makeItBold(startCount, endCount)}>Bold</button>
+        <button onClick={() => makeItItalic(startCount, endCount)}>
+          Italic
+        </button>
       </HeadEditor>
       <BodyEditor
         ref={editor}
         value={editorText}
         onChange={(e) => setEditorText(e.target.value)}
         onSelect={(e) => textSelector(e)}
-        autoComplete="off"
       ></BodyEditor>
-      <div>{editorText}</div>
+      <div dangerouslySetInnerHTML={{ __html: editorText }} />
     </Container>
   );
 };
