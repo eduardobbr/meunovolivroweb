4a5a219c973bc85f9cb62c4729899eeb435730c6
Guilherme Gonçalves <gcgoncalves13@gmail.com>
2024-01-30 17:56:03 -0300

    feat: add bold to text editor from draftjs

--- src/components/MnlEditor/index.jsx
+++ src/components/MnlEditor/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Editor, EditorState } from "draft-js";
+import { Editor, EditorState, RichUtils } from "draft-js";
 import "draft-js/dist/Draft.css";
 import { BodyEditor, Container, EditorTitle, HeadEditor } from "./style";
 
@@ -7,13 +7,20 @@ const MnlEditor = () => {
   const [editorState, setEditorState] = useState(() =>
     EditorState.createEmpty()
   );
+
+  const onBold = () => {
+    setEditorState(RichUtils.toggleInlineStyle(editorState, "BOLD"));
+  };
+
   useEffect(() => {
-    console.log(editorState);
+    console.log(editorState._immutable.currentContent.getPlainText());
   }, [editorState]);
+
   return (
     <Container>
       <HeadEditor>
         <EditorTitle>Editor MNL</EditorTitle>
+        <button onClick={onBold}>Bold</button>
       </HeadEditor>
       <BodyEditor>
         <Editor editorState={editorState} onChange={setEditorState}></Editor>
