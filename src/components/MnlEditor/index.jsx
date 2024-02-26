15d4a099ccdde3a40ac5e0f03b1c203414a01379
Guilherme Gonçalves <gcgoncalves13@gmail.com>
2024-02-26 17:29:39 -0300

    feat: mnl editor creates footnotes

--- src/components/MnlEditor/index.jsx
+++ src/components/MnlEditor/index.jsx
@@ -1,297 +1,113 @@
-import { useEffect, useRef, useState } from "react";
-import {
-  CompositeDecorator,
-  DefaultDraftBlockRenderMap,
-  Editor,
-  EditorState,
-  Modifier,
-  RichUtils,
-  SelectionState,
-} from "draft-js";
-import "draft-js/dist/Draft.css";
-import { BodyEditor, Container, EditorTitle, HeadEditor, Modal } from "./style";
-import { Map } from "immutable";
+import ReactQuill, { Quill } from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { useBooks } from "../../provider/Books";
 import { v4 as uuidv4 } from "uuid";
-import { Link, LinkRedirect, Title } from "./classBlocks";
-import { stateFromHTML } from "draft-js-import-html";
 
-const MnlEditor = ({ changer, bookContent }) => {
-  const [editorState, setEditorState] = useState(() =>
-    EditorState.createEmpty()
-  );
-  const [selectionState, setSelectionState] = useState();
-  const [showModal, setShowModal] = useState(false);
-  const [showLinkModal, setShowLinkModal] = useState(false);
-  const [link, setLink] = useState("");
-  const [linkText, setLinkText] = useState("");
-  const [endNoteText, setEndNoteText] = useState("");
-  const [endNotesCounter, setEndNotesCounter] = useState(1);
+let endNoteCounter = 1;
 
-  //BlockRender
-  const blockRenderMap = Map({
-    "header-one": {
-      element: Title,
-    },
-    "header-two": {
-      element: "h2",
-    },
-    unstyled: {
-      element: "div",
-    },
+// Criação de um ícone para o botão de nota de fim
+const CustomButton = () => '<span className="octicon octicon-star">N</span>';
+
+// Função para adicionar a nota de fim
+function insertEndnote(quill) {
+  const range = quill.getSelection();
+  const firstId = uuidv4();
+  const secondId = uuidv4();
+  let value = prompt("Insira o texto da nota de fim");
+  console.log(quill);
+  if (value) {
+    const markLink = `<a contenteditable="true" id='endnoteMark-${firstId}' href='#endnote-${secondId}'>[${endNoteCounter}]</a>`;
+    quill.insertEmbed(range.index, "custom", markLink, Quill.sources.USER);
+    const endNoteLink = `<p contenteditable="true"><span>[${endNoteCounter}] - ${value}</span> <a id='endnote-${secondId}' href='#endnoteMark-${firstId}'>voltar</a></p>`;
+    quill.insertEmbed(
+      quill.getLength() - 1,
+      "custom",
+      endNoteLink,
+      Quill.sources.USER
+    );
+    endNoteCounter++;
+  }
+}
+
+// Registro do módulo personalizado
+function registerCustomButton() {
+  const CustomBlotButton = function () {
+    const button = document.createElement("button");
+    button.className = "ql-endnote";
+    button.innerHTML = CustomButton();
+    button.onclick = () => insertEndnote(this);
+    return button;
+  };
+
+  Quill.register("modules/endNoteButton", function (quill, options) {
+    const toolbar = quill.getModule("toolbar");
+    toolbar.addHandler("endnote", () => insertEndnote(quill));
+    toolbar.container.appendChild(CustomBlotButton.call(quill));
   });
+}
 
-  const extendedBlockRender = DefaultDraftBlockRenderMap.merge(blockRenderMap);
+registerCustomButton();
 
-  //Button press functions
-  const onBold = () => {
-    setEditorState(RichUtils.toggleInlineStyle(editorState, "BOLD"));
-  };
-  const onItalic = () => {
-    setEditorState(RichUtils.toggleInlineStyle(editorState, "ITALIC"));
-  };
-  const onTitle = () => {
-    setEditorState(RichUtils.toggleBlockType(editorState, "header-one"));
-  };
-  const onSubTitle = () => {
-    setEditorState(RichUtils.toggleBlockType(editorState, "header-two"));
-  };
-  const onOl = () => {
-    setEditorState(RichUtils.toggleBlockType(editorState, "ordered-list-item"));
-  };
-  const onUl = () => {
-    setEditorState(
-      RichUtils.toggleBlockType(editorState, "unordered-list-item")
-    );
-  };
-  const onFootNote = () => {
-    setShowModal(true);
-  };
-  const onLink = () => {
-    setShowLinkModal(true);
-  };
+const CustomBlot = Quill.import("blots/embed");
 
-  //Entities find Functions for decorators
-  const findLinkEntities = (contentBlock, callback, contentState) => {
-    contentBlock.findEntityRanges((character) => {
-      const entityKey = character.getEntity();
-      return (
-        entityKey !== null &&
-        contentState.getEntity(entityKey).getType() === "LINK"
-      );
-    }, callback);
-  };
-
-  //Decorators for generate blocks
-  const createLinkDecorator = () =>
-    new CompositeDecorator([
-      {
-        strategy: findLinkEntities,
-        component: Link,
-      },
-    ]);
-  const createLinkRedirectDecorator = () =>
-    new CompositeDecorator([
-      {
-        strategy: findLinkEntities,
-        component: LinkRedirect,
-      },
-    ]);
-
-  //FootNotes Creattion
-  const handleCreateFootnote = () => {
-    const decorator = createLinkDecorator();
-    const contentState = editorState.getCurrentContent();
-
-    const firstId = uuidv4();
-    const secondId = uuidv4();
-
-    const entityMark = contentState.createEntity("LINK", "MUTABLE", {
-      url: `#endnote-${secondId}`,
-      id: `endnote-mark-${firstId}`,
-    });
-
-    const entityKey = entityMark.getLastCreatedEntityKey();
-
-    const textWithEntity = Modifier.insertText(
-      contentState,
-      selectionState,
-      `[${endNotesCounter}]`,
-      null,
-      entityKey
-    );
-
-    const newEditorState = EditorState.createWithContent(
-      textWithEntity,
-      decorator
-    );
-
-    const newContent = newEditorState.getCurrentContent();
-
-    const blockMap = newContent.getBlockMap();
-
-    const key = blockMap.last().getKey();
-
-    const length = blockMap.last().getLength();
-
-    const selection = new SelectionState({
-      anchorKey: key,
-      anchorOffset: length,
-      focusKey: key,
-      focusOffset: length,
-    });
-
-    const entityEndNote = contentState.createEntity("LINK", "MUTABLE", {
-      url: `#endnote-mark-${firstId}`,
-      id: `endnote-${secondId}`,
-    });
-
-    const endNoteKey = entityEndNote.getLastCreatedEntityKey();
-
-    const newEndNote = Modifier.insertText(
-      newContent,
-      selection,
-      `\n[${endNotesCounter}]`,
-      null,
-      endNoteKey
-    );
-
-    const endEditorState = EditorState.createWithContent(newEndNote, decorator);
-
-    const newContentWithEndNote = endEditorState.getCurrentContent();
-
-    const newBlockMap = newContentWithEndNote.getBlockMap();
-
-    const newKey = newBlockMap.last().getKey();
-
-    const newLength = newBlockMap.last().getLength();
-
-    const newSelection = new SelectionState({
-      anchorKey: newKey,
-      anchorOffset: newLength,
-      focusKey: newKey,
-      focusOffset: newLength,
-    });
-
-    const newEndNoteText = Modifier.insertText(
-      newContentWithEndNote,
-      newSelection,
-      ` - ${endNoteText}`
-    );
-
-    const newEndEditorState = EditorState.push(endEditorState, newEndNoteText);
-
-    setEditorState(newEndEditorState);
-    setEndNotesCounter(endNotesCounter + 1);
-    setEndNoteText("");
-  };
-
-  const handleCloseModal = (e) => {
-    if (e.target === e.currentTarget) {
-      setShowModal(false);
-      setShowLinkModal(false);
+class CustomBlotFormat extends CustomBlot {
+  static create(value) {
+    let node = super.create();
+    node.contentEditable = true;
+    if (typeof value === "object") {
+      node.innerHTML = value.content;
+    } else {
+      node.innerHTML = value;
     }
-  };
+    return node;
+  }
 
-  //Link Creation
-  const handleCreateLink = () => {
-    const decorator = createLinkRedirectDecorator();
-    const contentState = editorState.getCurrentContent();
+  static value(node) {
+    return {
+      content: node.innerHTML,
+    };
+  }
+}
+CustomBlotFormat.blotName = "custom";
+CustomBlotFormat.tagName = "span";
 
-    const entityLink = contentState.createEntity("LINK", "MUTABLE", {
-      url: link,
-    });
+Quill.register(CustomBlotFormat);
 
-    const entityKey = entityLink.getLastCreatedEntityKey();
+const MnlEditor = () => {
+  const { bookContent, setBookContent } = useBooks();
 
-    const newEndNoteText = Modifier.insertText(
-      contentState,
-      selectionState,
-      linkText,
-      null,
-      entityKey
-    );
-
-    const newEditorState = EditorState.createWithContent(
-      newEndNoteText,
-      decorator
-    );
-
-    setEditorState(newEditorState);
-    setLink("");
-    setLinkText("");
-    setShowLinkModal(false);
-  };
-
-  const editor = useRef();
-
-  //Get Selection
-  useEffect(() => {
-    setSelectionState(editorState.getSelection());
-  }, [editorState]);
-
-  //Editor load book
-  useEffect(() => {
-    if (bookContent) {
-      const fromHtml = stateFromHTML(bookContent);
-      const newEditor = EditorState.createWithContent(fromHtml);
-      console.log(fromHtml);
-      setEditorState(newEditor);
-    }
-  }, []);
-
-  const handleOnChange = (ev) => {
-    changer(editor.current.editor.innerHTML);
-    setEditorState(ev);
+  const modules = {
+    toolbar: {
+      container: [
+        [{ header: [1, 2, false] }],
+        ["bold", "italic", "underline", "strike", "blockquote"],
+        [
+          { list: "ordered" },
+          { list: "bullet" },
+          { align: ["justify", "", "center", "right"] },
+          { indent: "-1" },
+          { indent: "+1" },
+        ],
+        ["link", "image"],
+        ["endnote"], // Adicionar botão personalizado na barra de ferramentas
+      ],
+      handlers: {
+        endnote: insertEndnote,
+      },
+    },
+    endNoteButton: true, // Ativando o módulo personalizado
   };
 
   return (
-    <Container>
-      {showModal && (
-        <Modal onClick={(e) => handleCloseModal(e)}>
-          <div>
-            <h2>Insira o texto da sua nota de rodapé</h2>
-            <textarea
-              onChange={(e) => setEndNoteText(e.target.value)}
-            ></textarea>
-            <button onClick={() => setShowModal(false)}>Cancelar</button>
-            <button onClick={() => handleCreateFootnote()}>Criar</button>
-          </div>
-        </Modal>
-      )}
-
-      {showLinkModal && (
-        <Modal onClick={(e) => handleCloseModal(e)}>
-          <div>
-            <h2>Insira o texto do link</h2>
-            <textarea onChange={(e) => setLinkText(e.target.value)}></textarea>
-            <h2>Insira o Link</h2>
-            <textarea onChange={(e) => setLink(e.target.value)}></textarea>
-            <button onClick={() => setShowLinkModal(false)}>Cancelar</button>
-            <button onClick={() => handleCreateLink()}>Criar</button>
-          </div>
-        </Modal>
-      )}
-      <HeadEditor>
-        <EditorTitle>Editor MNL</EditorTitle>
-        <button onClick={onBold}>Bold</button>
-        <button onClick={onItalic}>Italic</button>
-        <button onClick={onTitle}>Title</button>
-        <button onClick={onSubTitle}>SubTitle</button>
-        <button onClick={onOl}>OL</button>
-        <button onClick={onUl}>UL</button>
-        <button onClick={onFootNote}>Make EndNote</button>
-        <button onClick={onLink}>Link</button>
-      </HeadEditor>
-      <BodyEditor>
-        <Editor
-          editorState={editorState}
-          onChange={handleOnChange}
-          blockRenderMap={extendedBlockRender}
-          classList="editor"
-          ref={editor}
-        ></Editor>
-      </BodyEditor>
-    </Container>
+    <div>
+      <ReactQuill
+        theme="snow"
+        modules={modules}
+        value={bookContent}
+        onChange={setBookContent}
+        style={{ height: "450px" }}
+      />
+    </div>
   );
 };
 
--- src/pages/Plans/index.jsx
+++ src/pages/Plans/index.jsx
@@ -1,82 +1,9 @@
 import Header from "../../components/Header";
-import MnlEditor from "../../components/MnlEditor";
-
-import React, { useState } from "react";
-import ReactQuill, { Quill } from "react-quill";
-import "react-quill/dist/quill.snow.css";
-
-// Criação de um ícone para o botão de nota de fim
-const CustomButton = () => '<span className="octicon octicon-star">N</span>';
-
-// Função para adicionar a nota de fim
-function insertEndnote(quill) {
-  const range = quill.getSelection();
-  let value = prompt("Insira o texto da nota de fim");
-  if (value) {
-    quill.insertText(range.index, ` [${value}] `, Quill.sources.USER);
-  }
-}
-
-// Registro do módulo personalizado
-function registerCustomButton() {
-  const CustomBlotButton = function () {
-    const button = document.createElement("button");
-    button.className = "ql-endnote";
-    button.innerHTML = CustomButton();
-    button.onclick = () => insertEndnote(this);
-    return button;
-  };
-
-  Quill.register("modules/endNoteButton", function (quill, options) {
-    const toolbar = quill.getModule("toolbar");
-    toolbar.addHandler("endnote", () => insertEndnote(quill));
-    toolbar.container.appendChild(CustomBlotButton.call(quill));
-  });
-}
-
-registerCustomButton();
-
-const EditorDeNotas = () => {
-  const [editorHtml, setEditorHtml] = useState("");
-
-  const modules = {
-    toolbar: {
-      container: [
-        [{ header: [1, 2, false] }],
-        ["bold", "italic", "underline", "strike", "blockquote"],
-        [
-          { list: "ordered" },
-          { list: "bullet" },
-          { indent: "-1" },
-          { indent: "+1" },
-        ],
-        ["link", "image"],
-        ["endnote"], // Adicionar botão personalizado na barra de ferramentas
-      ],
-      handlers: {
-        endnote: insertEndnote,
-      },
-    },
-    endNoteButton: true, // Ativando o módulo personalizado
-  };
-
-  return (
-    <div>
-      <ReactQuill
-        theme="snow"
-        modules={modules}
-        value={editorHtml}
-        onChange={setEditorHtml}
-      />
-    </div>
-  );
-};
 
 const Plans = () => {
   return (
     <>
       <Header />
-      <EditorDeNotas />
     </>
   );
 };
