0f709ce769d1b06505a3d0d76e73b7cee8c0ee01
Guilherme Gonçalves <gcgoncalves13@gmail.com>
2024-06-13 12:28:30 -0300

    feat: add toastfy

--- src/index.js
+++ src/index.js
@@ -7,12 +7,15 @@ import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme";
 import { GlobalStyle } from "./styles/globalStyle";
 import Provider from "./provider";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <ToastContainer />
       <Provider>
         <RouterProvider router={router} />
       </Provider>
